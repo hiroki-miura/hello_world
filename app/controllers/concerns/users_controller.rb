eae1e1893cf2e2a3bd2f3c7245c952876620d6a5
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-08-27 22:15:45 +0900

    add new greet

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    message = ["Hello World","Good Morning","Good Night"]
-    @message = message[1]
+    message = ["Hello World","Good Morning","Good Afternoon,""Good Evening","Good Night"]
+    @message = message[0]
   end
 end
